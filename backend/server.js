ea8994fe6dc18263c6dc7020867441790fa975fc
rob587 <robertocamm02@yahoo.com>
2026-08-27 13:02:04 +0200

    Refactor dotenv configuration: move dotenv.config() to server.js and database.js, remove env.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./src/app.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,6 @@
+import dotenv from "dotenv";
 import mysql from "mysql2/promise";
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
